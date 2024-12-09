b873e7bc42531e5b19058e5ec21222278646fefc
KessNelly <ojkessnelly@gmail.com>
2024-12-09 21:00:43 +0100

    appropriate routes for crud operation for account

--- src/application/routes/account.route.ts
+++ src/application/routes/account.route.ts
@@ -1,5 +1,21 @@
-import express, { Router } from 'express';
-import { AccountController } from '../controllers/account.controller';
+import { createAccountValidation } from "@/domains/account/accountValidation";
+import { Router } from "express";
+import { AccountController } from "../controllers/account.controller";
+//import { Router } from "express";
+//import { AccountController } from "../controllers/account.controller";
+import express from 'express';
+import { validateRequest } from "../middlewares/error-handler";
+
+
+const accountRouter = Router();
+
+accountRouter.post("/", validateRequest(createAccountValidation), AccountController.create);
+accountRouter.get("/:id", AccountController.getById);
+accountRouter.get("/", AccountController.getAll);
+accountRouter.put("/:id", AccountController.update);
+accountRouter.delete("/:id", AccountController.delete);
+
+export default accountRouter;
 
 const router: Router = express.Router();
 
