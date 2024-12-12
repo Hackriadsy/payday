9ff16499caac78c73a887b7ffa506dff57f398fc
KessNelly <ojkessnelly@gmail.com>
2024-12-12 11:39:15 +0100

    added subscription router to index file

--- src/index.ts
+++ src/index.ts
@@ -4,6 +4,7 @@ import setup from './application/middlewares';
 import { logger } from './core';
 
 import accountRouter from './application/routes/account.route';
+import subscriptionRouter from './application/routes/subscription.route';
 
 const app: Express = express();
 const port = process.env.PORT || 3000;
@@ -14,6 +15,7 @@ setup(app);
 app.use(express.json())
 
 app.use("/account", accountRouter)
+app.use("/subscription", subscriptionRouter)
 
 app.listen(port, () => {
   logger.info(`Server is running at http://localhost:${port}`);
