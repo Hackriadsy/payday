8d8b8788f6ae4e638cb026bfb0fdeb46ccc083f2
KessNelly <ojkessnelly@gmail.com>
2024-12-12 11:35:53 +0100

    added validation to update account

--- src/domains/account/accountValidation.ts
+++ src/domains/account/accountValidation.ts
@@ -19,3 +19,24 @@ export const createAccountValidation = [
     .isString()
     .withMessage("Avatar must be a valid string URL"),
 ];
+
+
+export const updateAccountValidation = [
+  body("name").optional().isString().withMessage("Name is required"),
+  body("phone").optional().isString(),
+  body("type")
+    .optional()
+    .isString()
+    .isIn(["individual", "business"])
+    .withMessage("Type must be 'individual' or 'business'"),
+  body("country")
+    .optional()
+    .isString()
+    .isLength({ min: 2, max: 2 })
+    .withMessage("Country must be a valid ISO 3166-1 alpha-2 code"),
+  body("avatar")
+    .optional()
+    .isString()
+    .withMessage("Avatar must be a valid string URL"),
+];
+
